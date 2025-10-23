b43ff4f5e00016cf96fd2a8aef72b1ccfc56d566
fre2dom0 <fre2dom0@gmail.com>
2025-10-23 07:46:51 +0300

    Fix: devLog message fixed

--- WealthOS_Backend/Servant_API/src/middlewares/isOwnerAddress.middleware.ts
+++ WealthOS_Backend/Servant_API/src/middlewares/isOwnerAddress.middleware.ts
@@ -5,7 +5,7 @@ import { devLog } from "../utils/consoleLoggers.util.js";
 
 export const isOwnerAddress = (req: Request, res: Response, next: NextFunction) => {
     try {
-        devLog(`${req.session.address} is not owner.`, 'INFO');
+        devLog(`Is ${req.session.address} owner : ${req.session.address == ADDRESS_CONFIG.owner_account_address}`, 'INFO');
         if (req.session.address != ADDRESS_CONFIG.owner_account_address) throw new ApiError('Only owner can access.', 'UNAUTHORIZED');
         next();
     } catch (err: unknown) {
