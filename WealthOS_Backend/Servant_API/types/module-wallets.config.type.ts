fe689033940a1cd7ebfebf36ed4e02ce11fc9e40
fre2dom0 <fre2dom0@gmail.com>
2025-10-29 23:49:29 +0300

    Update: node_key type added

--- WealthOS_Backend/Servant_API/types/module-wallets.config.type.ts
+++ WealthOS_Backend/Servant_API/types/module-wallets.config.type.ts
@@ -1,7 +1,8 @@
-export type BlockchainAddresses = {
+export type BlockchainConfig = {
     servant_contract_address: `0x${string}`;
     servant_account_address: `0x${string}`;
     servant_account_private_key: `0x${string}`;
     owner_account_address: `0x${string}`;
     owner_account_private_key: `0x${string}`;
+    node_key: string;
 }
