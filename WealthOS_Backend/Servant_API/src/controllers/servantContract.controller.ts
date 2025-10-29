c226a656edd108d7fc598911a498cdfe9dc82bdc
fre2dom0 <fre2dom0@gmail.com>
2025-10-29 23:48:04 +0300

    Update: Renamed to executor

--- WealthOS_Backend/Servant_API/src/controllers/servantContract.controller.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response, NextFunction } from "express"
-
-export const servantExecuteService = (req: Request, res: Response, next: NextFunction) => {
-    try {
-        
-    } catch (err: unknown) {
-        next(err);
-    }
-}
