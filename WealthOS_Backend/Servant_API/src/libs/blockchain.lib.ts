67e0905f46b34a2a5ebb7ca7da14628e48aeb65e
fre2dom0 <fre2dom0@gmail.com>
2025-10-29 23:47:06 +0300

    Update: Simplified and created execute function for executor

--- WealthOS_Backend/Servant_API/src/libs/blockchain.lib.ts
+++ WealthOS_Backend/Servant_API/src/libs/blockchain.lib.ts
@@ -1,45 +1,34 @@
-import { createPublicClient, createWalletClient, getContract, http, type Abi, type AbiItem, type Client, type GetContractReturnType, type HttpTransport } from 'viem'
+import { createPublicClient, createWalletClient, getContract, http, type Abi, type Client, type GetContractReturnType, type HttpTransport } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import { sepolia } from 'viem/chains'
-import { ADDRESS_CONFIG } from '../configs/chain.config.js'
-import { ApiError } from '../errors/ApiError.error.js'
+import { CHAIN_CONFIG } from '../configs/chain.config.js'
+import servant_abi from '../configs/jsons/servant_artifact.json' with {type: 'json'};
+import { devLog } from '../utils/consoleLoggers.util.js';
 
-export const ABI = [{ "inputs": [], "name": "DEFAULT_ADMIN_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "MAX_APPROVAL_TIME", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "MIN_APPROVAL_TIME", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "SERVANT_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "UPGRADE_INTERFACE_VERSION", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "allFnsApproved", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "time", "type": "uint256" }, { "internalType": "bytes4[]", "name": "addedfnSelectors", "type": "bytes4[]" }], "name": "approve", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address[]", "name": "modules", "type": "address[]" }], "name": "authorizeModule", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "authorizedModules", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }, { "internalType": "address", "name": "module", "type": "address" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "name": "execute", "outputs": [{ "internalType": "bytes", "name": "returnData", "type": "bytes" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }], "name": "getRoleAdmin", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "grantRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "hasRole", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "trustedForwarder_", "type": "address" }, { "internalType": "address", "name": "_servant", "type": "address" }], "name": "initialize", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }, { "internalType": "bytes4", "name": "", "type": "bytes4" }], "name": "isFnApproved", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "forwarder", "type": "address" }], "name": "isTrustedForwarder", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "proxiableUUID", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "callerConfirmation", "type": "address" }], "name": "renounceRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "revoke", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes4[]", "name": "removedFnSelectors", "type": "bytes4[]" }], "name": "revokeFunctions", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "revokeRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" }], "name": "supportsInterface", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "trustedForwarder", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address[]", "name": "modules", "type": "address[]" }], "name": "unauthorizeModule", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newImplementation", "type": "address" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "name": "upgradeToAndCall", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "userApprovalExpiry", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "pure", "type": "function" }] as const
 
-/**
- * Creates Web3 client
- * @param private_key Private key of account
- * @returns Client
- */
-export const createBlockchainClient = (private_key: `0x${string}`): Client<HttpTransport<undefined, false>> => {
+
+export const publicClient = createPublicClient({ chain: sepolia, transport: http(CHAIN_CONFIG.node_key) });
+export const walletClient = createWalletClient({
+    chain: sepolia,
+    transport: http(CHAIN_CONFIG.node_key)
+})
+
+export const execute = async (args: any[]): Promise<`0x${string}`> => {
     try {
-        const account = privateKeyToAccount(private_key)
-        const walletClient = createWalletClient({
-            account,
-            chain: sepolia,
-            transport: http()
+        devLog(`Simulating the execution with args: ${args}`, 'INFO');
+        const { request } = await publicClient.simulateContract({
+            address: CHAIN_CONFIG.servant_contract_address,
+            abi: servant_abi.abi,
+            functionName: 'execute',
+            args,
+            account: privateKeyToAccount(CHAIN_CONFIG.servant_account_private_key)
         })
+        
 
-        return walletClient;
-
+        const hash = await walletClient.writeContract(request);
+        devLog(`Transaction successfully realized: ${hash}`, 'SUCCESS');
+        return hash;
     } catch (err: unknown) {
-        ApiError.fatalError(err);
+        throw err;
     }
 }
-
-export const publicClient = createPublicClient({ chain: sepolia, transport: http() });
-
-export const servantClient: Client<HttpTransport<undefined, false>> = createBlockchainClient(ADDRESS_CONFIG.servant_account_private_key);
-export const ownerClient: Client<HttpTransport<undefined, false>> = createBlockchainClient(ADDRESS_CONFIG.owner_account_private_key);
-
-export const servantContractWithServantClient = getContract({
-    address: ADDRESS_CONFIG.servant_contract_address,
-    abi: ABI,
-    client: { wallet: servantClient, public: publicClient }
-})
-
-export const servantContractWithOwnerCLient = getContract({
-    address: ADDRESS_CONFIG.servant_contract_address,
-    abi: ABI,
-    client: { wallet: ownerClient, public: publicClient }
-})
